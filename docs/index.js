78af4a97f079ac3c045042be74f956361e12695c
Tim Zadorozhny <tzador@gmail.com>
2021-02-19 20:52:04 +0200

    public-private-ws

--- docs/index.js
+++ docs/index.js
@@ -1,35 +1,108 @@
-const TOKEN = "token";
+const TOKEN = "auth_token";
+const public_ws_url = "ws://localhost:8080/ws/public";
+const private_ws_url = "ws://localhost:8080/ws/private";
+const binance_ws_url = "wss://stream.binance.com:9443/ws/btcusdt@aggTrade";
+const reconnect_timeout_ms = 1000;
 
 const state = {
   token: localStorage.getItem(TOKEN) || null,
   width: 0,
   height: 0,
+  history: [],
+  ticker: { ticker: "btcusdt", seconds: 0, price: 0 },
 };
+let public_ws = null;
+let private_ws = null;
 
-// Auth
+reconnect_public_ws();
+reconnect_private_ws();
+
+// # Auth
 function login() {
   state.token = prompt("Enter a nickname:") || null;
   if (state.token) {
     state.token = state.token.trim();
     localStorage.setItem(TOKEN, state.token);
   } else localStorage.removeItem(TOKEN);
+  reconnect_private_ws();
 }
 
 function logout() {
   state.token = null;
   localStorage.removeItem(TOKEN);
+  reconnect_private_ws();
 }
 
-const logout_a = document.getElementById("logout");
+const ticker_el = document.getElementById("ticker");
+const login_el = document.getElementById("login");
+const logout_el = document.getElementById("logout");
+let last_ticker_value = null;
 
-(function update_login_logout() {
-  requestAnimationFrame(update_login_logout);
+(function mirror() {
   if (state.token) {
-    logout_a.innerText = "Logout @" + state.token;
+    login_el.style.display = "none";
+    logout_el.style.display = "block";
+    logout_el.innerText = "Logout @" + state.token;
   } else {
-    logout_a.style.innerText = "";
+    login_el.style.display = "block";
+    logout_el.style.display = "none";
+    logout_el.style.innerText = "";
   }
+
+  {
+    // Ticker
+    const date = new Date(state.ticker.seconds * 1000)
+      .toJSON()
+      .replace(".000", "")
+      .replace("T", " ")
+      .replace("Z", "");
+    let price_str = state.ticker.price.toString();
+    while (price_str.length < 9) price_str = price_str + "0";
+    const ticker_value = `${date} &nbsp; 1 BTC = <span style="color: white">${price_str}</span> USDT`;
+    if (ticker_value != last_ticker_value) ticker_el.innerHTML = ticker_value;
+    last_ticker_value = ticker_value;
+  }
+  JSON.stringify(state.ticker);
+  requestAnimationFrame(mirror);
 })();
 
-const ws_url = "ws://localhost:7777/ws";
-const ws = new WebSocket(ws_url);
+// # WebSocket
+function reconnect_public_ws() {
+  if (public_ws) public_ws.close();
+  public_ws = new WebSocket(public_ws_url);
+  public_ws.onclose = () =>
+    setTimeout(reconnect_public_ws, reconnect_timeout_ms);
+  public_ws.onerror = (error) => {
+    console.error(error);
+    setTimeout(reconnect_public_ws, reconnect_timeout_ms);
+  };
+  public_ws.onmessage = (message) => {
+    const { tag, data } = JSON.parse(message.data);
+    on_public(tag, data);
+  };
+}
+
+function reconnect_private_ws() {
+  if (private_ws) private_ws.close();
+  private = null;
+  if (!state.token) return;
+  private_ws = new WebSocket(private_ws_url);
+  private_ws.onclose = () =>
+    setTimeout(reconnect_private_ws, reconnect_timeout_ms);
+  private_ws.onerror = (error) => {
+    console.error(error);
+    setTimeout(reconnect_private_ws, reconnect_timeout_ms);
+  };
+  private_ws.onmessage = (message) => {
+    const { tag, data } = JSON.parse(message.data);
+    on_private(tag, data);
+  };
+}
+
+function on_public(tag, data) {
+  if (tag == "ticker#btcusdt") state.ticker = data;
+}
+
+function on_private(tag, data) {
+  console.log("on_private", tag, data);
+}
--- worker/main.js
+++ worker/main.js
@@ -4,9 +4,8 @@ const WebSocket = require("ws");
 const Redis = require("ioredis");
 
 const port = 8080;
-const BINANCE_WS_URL = "wss://stream.binance.com:9443/ws/btcusdt@aggTrade";
-
-const redis = new Redis(process.env.REDIS_URL);
+const binance_ws_url = "wss://stream.binance.com:9443/ws/btcusdt@aggTrade";
+const history_length = 6;
 
 const app = express();
 app.set("json spaces", 2);
@@ -15,49 +14,105 @@ app.use(express.static("docs"));
 const server = http.createServer(app);
 server.listen(port, () => console.log("http://localhost:" + port));
 
-const wss = new WebSocket.Server({ noServer: true });
+const wss_public = new WebSocket.Server({ noServer: true });
+const wss_private = new WebSocket.Server({ noServer: true });
+
 server.on("upgrade", function upgrade(req, socket, head) {
-  if (req.url === "/ws") {
-    wss.handleUpgrade(req, socket, head, (ws) => {
-      wss.emit("connection", ws, req);
+  if (req.url === "/ws/public") {
+    wss_public.handleUpgrade(req, socket, head, (ws) => {
+      wss_public.emit("connection", ws, req);
+    });
+  } else if (req.url === "/ws/private") {
+    wss_private.handleUpgrade(req, socket, head, (ws) => {
+      wss_private.emit("connection", ws, req);
     });
   } else {
     socket.destroy();
   }
 });
 
-// REST API
-app.get("/rest/ticker/btcusdt", async (req, res) => {
-  const json = JSON.parse(await redis.get("ticker#btcusdt"));
-  res.json(json);
+// # Store
+class RedisStore {
+  constructor(redis) {
+    this.redis = redis;
+  }
+
+  async set_ticker(ticker, seconds, price) {
+    const json = JSON.stringify({ ticker, seconds, price });
+    await this.redis.set("ticker#" + ticker, json);
+    await this.redis.zadd("history#" + ticker, 1, json);
+  }
+
+  async get_ticker(ticker) {
+    const value = await this.redis.get("ticker#" + ticker);
+    return JSON.parse(value);
+  }
+
+  async get_history(ticker) {
+    let start = 0;
+    start = Math.floor(Date.now() / 1000) - 1000;
+    const values = await this.redis.zrange(
+      "history#" + ticker,
+      -history_length,
+      -1
+    );
+    return values.map(JSON.parse);
+  }
+}
+const store = new RedisStore(new Redis(process.env.REDIS_URL));
+
+// # REST API
+app.get("/rest/ticker/:ticker", async (req, res) => {
+  res.json(await store.get_ticker(req.params.ticker));
 });
 
-// WebSocket API
-wss.on("connection", function connection(ws) {
-  ws.send(JSON.stringify("hello"));
+app.get("/rest/history/:ticker", async (req, res) => {
+  res.json(await store.get_history(req.params.ticker));
 });
 
-// Binance
+// # Public WebSocket API
+wss_public.on("connection", function connection(ws) {
+  ws.send(JSON.stringify({ tag: "hello", data: "public" }));
+});
+function wss_public_broadcast(tag, data) {
+  for (const ws of wss_public.clients) {
+    if (ws.readyState === WebSocket.OPEN) {
+      ws.send(JSON.stringify({ tag, data }));
+    }
+  }
+}
+
+// # Private WebSocket API
+wss_private.on("connection", function connection(ws) {
+  ws.send(JSON.stringify({ tag: "hello", data: "todo@private" }));
+});
+function wss_private_broadcast(user_id, tag, data) {}
+
+// # Binance
 (function binance() {
+  const ticker = "btcusdt";
   const queue = [];
   let last_time = 0;
   let last_price = 0;
   (async function process_binance() {
-    const tick = queue.shift();
-    if (tick) {
-      const { time, price } = tick;
+    while (queue.length) {
+      const { time, price } = queue.shift();
       if (last_time && Math.floor(last_time) != Math.floor(time)) {
         const seconds = Math.floor(time);
-        const json = JSON.stringify({ seconds, last_price });
-        await redis.set("ticker#btcusdt", json);
+        store.set_ticker(ticker, seconds, last_price);
+        wss_public_broadcast("ticker#" + ticker, {
+          ticker,
+          seconds,
+          price: last_price,
+        });
       }
       last_time = time;
       last_price = price;
     }
-    setTimeout(process_binance); // TODO replace with process.nextTick
+    setTimeout(process_binance, 1); // TODO replace with process.nextTick
   })();
   (async function connect_to_binance() {
-    const ws = new WebSocket(BINANCE_WS_URL);
+    const ws = new WebSocket(binance_ws_url);
     ws.on("close", () => setTimeout(connect_to_binance, 100));
     ws.on("message", (message) => {
       const data = JSON.parse(message);

2ade32e6056ab43f610508e24417ef785bb72362
Tim Zadorozhny <tzador@gmail.com>
2021-02-19 17:41:09 +0200

    cleanup

--- docs/index.js
+++ docs/index.js
@@ -1,2 +1,35 @@
+const AUTH_KEY = "auth-key";
+
+const state = {
+  user: localStorage.getItem(AUTH_KEY) || null,
+  width: 0,
+  height: 0,
+};
+
+// Auth
+function login() {
+  state.user = prompt("Enter a nickname:") || null;
+  if (state.user) {
+    state.user = state.user.trim();
+    localStorage.setItem(AUTH_KEY, state.user);
+  } else localStorage.removeItem(AUTH_KEY);
+}
+
+function logout() {
+  state.user = null;
+  localStorage.removeItem(AUTH_KEY);
+}
+
+const logout_a = document.getElementById("logout");
+
+(function update_login_logout() {
+  requestAnimationFrame(update_login_logout);
+  if (state.user) {
+    logout_a.innerText = "Logout @" + state.user;
+  } else {
+    logout_a.style.innerText = "";
+  }
+})();
+
 const ws_url = "ws://localhost:7777/ws";
 const ws = new WebSocket(ws_url);
--- worker/main.js
+++ worker/main.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const http = require("http");
-const WS = require("ws");
+const WebSocket = require("ws");
 const Redis = require("ioredis");
 
 const port = 8080;
+const BINANCE_URL = "wss://stream.binance.com:9443/ws/btcusdt@aggTrade";
 
 const redis = new Redis(process.env.REDIS_URL);
 
@@ -13,11 +14,9 @@ app.use(express.static("docs"));
 const server = http.createServer(app);
 server.listen(port, () => console.log("http://localhost:" + port));
 
-// HTTP API
 app.get("/api", (req, res) => res.json("api"));
 
-// Route WebSockets
-const wss = new WS.Server({ noServer: true });
+const wss = new WebSocket.Server({ noServer: true });
 server.on("upgrade", function upgrade(req, socket, head) {
   if (req.url === "/ws") {
     wss.handleUpgrade(req, socket, head, (ws) => {
@@ -31,3 +30,14 @@ server.on("upgrade", function upgrade(req, socket, head) {
 wss.on("connection", function connection(ws) {
   ws.send("hello");
 });
+
+(function binance() {
+  const ws = new WebSocket(BINANCE_URL);
+  ws.on("close", () => setTimeout(binance, 100));
+  ws.on("message", (message) => {
+    const data = JSON.parse(message);
+    const time = data.T / 1000.0;
+    const price = parseFloat(data.p);
+    console.log(time, price);
+  });
+})();

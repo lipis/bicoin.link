e9ebbcf960df72f46ca35bf789ed4bf53fb0bbd7
Tim Zadorozhny <tzador@gmail.com>
2021-02-20 10:51:25 +0200

    favicon.js

--- vanilla/favicon.js
+++ vanilla/favicon.js
@@ -1,1 +1,20 @@
-console.log("favicon.js");
+{
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+  const img = new Image();
+  const favicon = document.getElementById("favicon");
+  canvas.width = 64;
+  canvas.height = 64;
+  img.src = "favicon.png";
+  img.onload = function favicon_loop() {
+    ctx.save();
+    ctx.clearRect(0, 0, 64, 64);
+    ctx.translate(32, 32);
+    ctx.rotate(-performance.now() / 1000.0);
+    ctx.drawImage(img, 0, 0, img.width, img.height, -32, -32, 64, 64);
+    const base64 = canvas.toDataURL("image/png");
+    favicon.href = base64;
+    ctx.restore();
+    requestAnimationFrame(favicon_loop);
+  };
+}
